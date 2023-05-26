df0b6d5b7cb7076d965673bd0fa9a2b8e7d7aadd
Mohamed Amir <Mohamedamir5050@gmail.com>
2023-05-26 07:17:09 +0300

    Register in database done :)

--- virtualENV/studentAffairsProject/studentAffairsApp/urls.py
+++ virtualENV/studentAffairsProject/studentAffairsApp/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path, include
-urlpatterns = [
-    path('', views.index, name = 'home')
 
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('Home/', views.Home, name = 'Home'),
+    path('Register/', views.Register, name='Register'),
+    path('Register/addStudent', views.addStudent, name='addStudent'),
+    path('Login/', views.Login, name='Login'),
 ]
--- virtualENV/studentAffairsProject/studentAffairsApp/views.py
+++ virtualENV/studentAffairsProject/studentAffairsApp/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 def index(request):
     return render(request, 'index.html')
+
+def Home(request):
+    return render(request, 'Home.html')
+
+def Register(request):
+    return render(request, 'Register.html')
+
+def addStudent(request):
+    Name = request.POST["UserName"]
+    Id = request.POST["Id"]
+    email = request.POST["Email"]
+    Phone = request.POST["Phone"]
+    GPA = request.POST["GPA"]
+    Brith = request.POST["Brith"]
+    Level = request.POST["Level"]
+    Gender = request.POST["Gender"]
+    Department = request.POST["Department"]
+    Status = request.POST["Status"]
+    Password = request.POST["Password"]
+
+    student = Student(name = Name, id = Id, Email = email, phone = Phone, gpa = GPA,
+                      birthDate = Brith, gender = Gender, level = Level, status = Status,
+                      department = Department, password = Password)
+    
+    student.save()
+    return HttpResponse("Student added")
+    
+
+def Login(request):
+    return render(request, 'Login.html')
