61a5b906a81e0824fa71f22112a3c17b535c2274
Mohamed Amir <Mohamedamir5050@gmail.com>
2023-05-30 03:30:18 +0300

    Add validation by Ajax and jquary

--- virtualENV/studentAffairsProject/static/js/Register.js
+++ virtualENV/studentAffairsProject/static/js/Register.js
@@ -1,1 +1,87 @@
-// Ajax 
+$(document).ready(function() {
+    $.validator.addMethod("passwordMatch", function(value) {
+        let password = $("#Pass").val();
+        return password === value;
+    }, "Passwords do not match.");
+
+    $("#validateForm").validate({
+        rules: {
+            Name: {
+                required: true,
+                minlength: 3,
+                numeric: false
+            },
+            Email: {
+                email: true,
+                required: true
+            },
+            Id: {
+                required: true
+            },
+            Phone:{
+              required: true,
+              minlength: 11,
+              maxlength: 11
+            },
+            GPA:{
+                required: true
+            },
+            BirthDate:{
+                required: true
+            },
+            Level:{
+                required: true
+            },
+            Department:{
+                required: true
+            },
+            Password: {
+                required: true,
+                minlength: 8,
+                maxlength: 15
+            },
+            PassConf: {
+                required: true,
+                passwordMatch: true // Use the custom validation rule for password matching
+            }
+        },
+        messages: {
+            Name: {
+                required: "Please enter a valid name.",
+                minlength: "Name must be at least 3 characters."
+            },
+            Email:{
+                required: "Please enter an email.",
+                email: "Please enter valid email."
+            },
+            Phone: {
+                required: "Please enter a phone.",
+                minlength : "Please enter valid phone (11 digits).",
+                maxlength : "Please enter valid phone."
+            },
+            Id: {
+                required: "Please enter an id.",
+            },
+            GPA:{
+                required: "Please enter a valid GPA."
+            },
+            Department: {
+                required: "Please enter Department.",
+            },
+            BirthDate: {
+                required: "Please enter Birth date.",
+            },
+            Level: {
+                required: "Please enter Level.",
+            },
+            Password: {
+                required: "Please enter a password.",
+                minlength: "The password should be at least 8 characters.",
+                maxlength: "The password should be at most 15 characters."
+            },
+            PassConf: {
+                required: "Please confirm your password.",
+            }
+        }
+    });
+});
--- virtualENV/studentAffairsProject/studentAffairsApp/static/js/Register.js
+++ virtualENV/studentAffairsProject/studentAffairsApp/static/js/Register.js
@@ -1,72 +1,87 @@
-function Save(){
-  let Na = document.getElementById("Name").value ;
-  let id = document.getElementById("ID").value;
-  let Em = document.getElementById("E").value ;
-  let Ph = document.getElementById("Ph").value  ;
-  let Gpa = document.getElementById("GPA").value  ;
-  let Birth = document.getElementById("Dob").value ;
+$(document).ready(function() {
+    $.validator.addMethod("passwordMatch", function(value) {
+        let password = $("#Pass").val();
+        return password === value;
+    }, "Passwords do not match.");
 
-  const select = document.getElementById("g");
-  let G = select.options[select.selectedIndex].value  ;
-
-  let Lev = document.getElementById("Lev").value  ;
-
-  let stat = document.getElementsByName('Status');
-  let Stat ;
-  for(let i = 0; i < stat.length; i++) {
-    if(stat[i].checked) {
-      Stat = stat[i].value;
-    }
-  }
-  let Depart = document.getElementById("Depart").value  ;
-  let Pass = document.getElementById("Pass").value  ;
-  let PassConfirm = document.getElementById("PassConf").value  ;
-
-  if (Na === "" || id === "" || Em === "" || Ph === "" || Gpa === "" ||
-    Birth === "" || Lev === "" || Stat === "" || Depart === "" ||
-    Pass === "" || PassConfirm === ""){
-    swal("Empty fields", "Please enter all fields", "error", {
-      button: "try again",
+    $("#validateForm").validate({
+        rules: {
+            Name: {
+                required: true,
+                minlength: 3,
+                numeric: false
+            },
+            Email: {
+                email: true,
+                required: true
+            },
+            Id: {
+                required: true
+            },
+            Phone:{
+              required: true,
+              minlength: 11,
+              maxlength: 11
+            },
+            GPA:{
+                required: true
+            },
+            BirthDate:{
+                required: true
+            },
+            Level:{
+                required: true
+            },
+            Department:{
+                required: true
+            },
+            Password: {
+                required: true,
+                minlength: 8,
+                maxlength: 15
+            },
+            PassConf: {
+                required: true,
+                passwordMatch: true // Use the custom validation rule for password matching
+            }
+        },
+        messages: {
+            Name: {
+                required: "Please enter a valid name.",
+                minlength: "Name must be at least 3 characters."
+            },
+            Email:{
+                required: "Please enter an email.",
+                email: "Please enter valid email."
+            },
+            Phone: {
+                required: "Please enter a phone.",
+                minlength : "Please enter valid phone (11 digits).",
+                maxlength : "Please enter valid phone."
+            },
+            Id: {
+                required: "Please enter an id.",
+            },
+            GPA:{
+                required: "Please enter a valid GPA."
+            },
+            Department: {
+                required: "Please enter Department.",
+            },
+            BirthDate: {
+                required: "Please enter Birth date.",
+            },
+            Level: {
+                required: "Please enter Level.",
+            },
+            Password: {
+                required: "Please enter a password.",
+                minlength: "The password should be at least 8 characters.",
+                maxlength: "The password should be at most 15 characters."
+            },
+            PassConf: {
+                required: "Please confirm your password.",
+            }
+        }
     });
-  }
-  else{
-    if (Lev < 3 && Depart !== ""){
-      swal("Low Level", "Department in Level 3 or above", "error", {
-        button: "try again",
-      });
-    }
-    else{
-      let storageItems = localStorage.getItem(id);
-      if (storageItems !== null) {
-        swal("ID is already exist", "you should use an unique id", "error", {
-          button: "Ok",
-        });
-      }
-      else{
-        if (Pass !== PassConfirm){
-          swal("Passwords are not identical", "Please enter identical Passwords", "error", {
-            button: "try again",
-          });
-        }
-        else{
-          let Stud = {
-            Name: Na,
-            ID: id,
-            Email: Em,
-            Phone: Ph,
-            GPA: Gpa,
-            birth: Birth,
-            Gender: G,
-            Level: Lev,
-            Status: Stat,
-            Department: Depart,
-            Password: Pass
-          }
-
-          window.localStorage.setItem(id, JSON.stringify(Stud)) ;
-          window.location.href = "../HTML/Login.html" ;
-        }
-      }
-    }
-  }
-}
+});
